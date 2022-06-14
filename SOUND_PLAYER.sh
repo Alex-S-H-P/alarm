d82ac0019e0e1eb454354212061a809f9d76ebf5
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2022-06-14 10:00:47 +0200

    added a music player detection script

---
+++ SOUND_PLAYER.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if hash mpv 2>/dev/null; then
+    echo "mpv "
+elif hash vlc 2>/dev/null; then
+    echo "vlc "
+elif hash xdg-open 2>/dev/null; then
+    echo "xdg-open "
+elif hash rhythmbox 2>/dev/null; then
+    echo "rhythmbox "
+else
+    exit 1
+fi
